219dea86264805b10f2a4331e856281f9af76b43
raspberry-pi-ssw <softservedweb@gmail.com>
2025-10-05 23:03:37 +0530

    fit-to-page removed

--- ury_companion_ssw/ury_companion_ssw/overrides/api.py
+++ ury_companion_ssw/ury_companion_ssw/overrides/api.py
@@ -251,7 +251,7 @@ def network_printing_override(
                             "lp",
                             "-d", "ProPOS_PP9000EU",
                             "-o", "orientation-requested=3",  # portrait
-                            "-o", "fit-to-page",             # scale image to fill page
+                            # "-o", "fit-to-page",             # scale image to fill page
                             abs_path
                         ],
                         capture_output=True,
